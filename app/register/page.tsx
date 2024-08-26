29ad43e03653dcf9eee3ea0bfec81a79913826e6
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-26 15:08:46 +0900

    update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Register for a new account.</CardDescription>
+        </CardHeader>
+      </Card>
+    </main>
+  );
 };
 
 export default Register;
