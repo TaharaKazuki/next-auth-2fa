6ceeac231d5e8de7735513ecff44ded5a8216b9f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-26 15:30:08 +0900

    update

---
+++ app/register/_schema.ts
@@ -0,0 +1,9 @@
+import * as z from 'zod';
+
+export const registerFormSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(5),
+  passwordConfirm: z.string(),
+});
+
+export type RegisterFormSchemaType = z.infer<typeof registerFormSchema>;
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,38 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { registerFormSchema, RegisterFormSchemaType } from './_schema';
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 const Register = () => {
+  const form = useForm<RegisterFormSchemaType>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  });
+
+  const handleSubmit = async (data: RegisterFormSchemaType) => {};
+
   return (
     <main className="flex min-h-screen items-center justify-center">
       <Card className="w-[350px]">
@@ -13,6 +40,53 @@ const Register = () => {
           <CardTitle>Register</CardTitle>
           <CardDescription>Register for a new account.</CardDescription>
         </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="email" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="email" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="email" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </main>
   );
