5c2637488ebab300fa7d188dd35f6a0668a35d7f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-29 16:41:30 +0900

    update

--- app/(logged-out)/password-reset/page.tsx
+++ app/(logged-out)/password-reset/page.tsx
@@ -45,65 +45,80 @@ const PasswordReset = () => {
 
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Password Reset</CardTitle>
-          <CardDescription>
-            Enter your email address to reset your password.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleSubmit)} noValidate>
-              <fieldset
-                disabled={form.formState.isSubmitting}
-                className="flex flex-col gap-2"
-              >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          type="email"
-                          autoComplete="email"
-                          formNoValidate
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
+      {form.formState.isSubmitSuccessful ? (
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Email Sent</CardTitle>
+          </CardHeader>
+          <CardContent>
+            If you have an account with us you will receive a password reset
+            email at{' '}
+            <span className="font-semibold">{`"${form.getValues(
+              'email'
+            )}"`}</span>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Password Reset</CardTitle>
+            <CardDescription>
+              Enter your email address to reset your password.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)} noValidate>
+                <fieldset
+                  disabled={form.formState.isSubmitting}
+                  className="flex flex-col gap-2"
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            type="email"
+                            autoComplete="email"
+                            formNoValidate
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  {!!form.formState.errors.root?.message && (
+                    <FormMessage>
+                      {form.formState.errors.root.message}
+                    </FormMessage>
                   )}
-                />
+                  <Button type="submit">Submit</Button>
+                </fieldset>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <div className="text-sm text-muted-foreground">
+              Remember your password?{' '}
+              <Link href={'/login'} className="underline">
+                Login
+              </Link>
+            </div>
 
-                {!!form.formState.errors.root?.message && (
-                  <FormMessage>
-                    {form.formState.errors.root.message}
-                  </FormMessage>
-                )}
-                <Button type="submit">Submit</Button>
-              </fieldset>
-            </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <div className="text-sm text-muted-foreground">
-            Remember your password?{' '}
-            <Link href={'/login'} className="underline">
-              Login
-            </Link>
-          </div>
-
-          <div className="text-sm text-muted-foreground">
-            Don&apos;t have an account?{' '}
-            <Link href={'/register'} className="underline">
-              Register
-            </Link>
-          </div>
-        </CardFooter>
-      </Card>
+            <div className="text-sm text-muted-foreground">
+              Don&apos;t have an account?{' '}
+              <Link href={'/register'} className="underline">
+                Register
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      )}
     </main>
   );
 };
